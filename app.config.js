8cc2f00c928656f2464bc5edf1f739b91a0bac3f
alimahmudlu <alimahmudlu@gmail.com>
2025-09-01 09:26:30 +0400

    update app

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "EGB",
     "slug": "egb-expo-app",
-    "version": "0.0.35",
+    "version": "0.0.36",
     "orientation": "default",
     "icon": "./assets/images/icon-color.png",
     "scheme": "egbappexpo",
--- components/sections/DownloadApp/DownloadApp.jsx
+++ components/sections/DownloadApp/DownloadApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import {View, Text, TouchableOpacity, Platform, Linking} from 'react-native';
 import styles from '@/components/sections/DownloadApp/DownloadApp.styles';
 import PlayStoreIcon from '@/assets/images/Google Play logo.svg';
 import PlayStoreLabel from '@/assets/images/playlabel.svg';
@@ -10,22 +10,34 @@ const platformData = {
   android: {
     Icon: PlayStoreIcon,
     IconLabel: PlayStoreLabel,
+      href: 'https://play.google.com/store/apps/details?id=com.egb.egb',
   },
   ios: {
     Icon: AppStoreIcon,
     IconLabel: AppStoreLabel,
+      href: 'https://apps.apple.com/us/app/egbapp/id6747012814',
   },
 };
 
 export default function SgSectionDownloadApp({ version, title, platforms }) {
+
+    const openLink = async (url) => {
+        const supported = await Linking.canOpenURL(url);
+        if (supported) {
+            await Linking.openURL(url);
+        } else {
+            console.warn(`Don't know how to open this URL: ${url}`);
+        }
+    };
+
   const renderButtons = () => {
-  return platforms.map((platform) => {
+  return platforms.filter(el => Platform.OS === el).map((platform) => {
     const data = platformData[platform];
     if (!data) return null;
 
     const { Icon, IconLabel } = data;
         return (
-        <TouchableOpacity key={platform} style={styles.button}>
+        <TouchableOpacity key={platform} style={styles.button} onPress={() => openLink(data.href)}>
             <Icon style={styles.icon} />
             <IconLabel style={styles.icon} />
         </TouchableOpacity>
