bf61b6ba7ded3fb84c54f5663c7d8420474d2d5f
alimahmudlu <alimahmudlu@gmail.com>
2025-09-12 14:34:36 +0400

    update app

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "EGB",
     "slug": "egb-expo-app",
-    "version": "0.0.42",
+    "version": "0.0.43",
     "orientation": "default",
     "icon": "./assets/images/icon-color.png",
     "scheme": "egbappexpo",
