156093cad33ee5846a9efc3fb38263ca417bbfda
alimahmudlu <alimahmudlu@gmail.com>
2026-01-20 06:40:21 +0400

    update app

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
     "expo": {
         "name": "EGB",
         "slug": "egb-expo-app",
-        "version": "0.3.5",
+        "version": "0.3.6",
         "orientation": "default",
         "icon": "./assets/images/icon-color.png",
         "scheme": "egbappexpo",
@@ -33,7 +33,7 @@ export default {
             package: "com.egb.egb",
             edgeToEdgeEnabled: true,
             usesCleartextTraffic: true,
-            versionCode: 44,
+            versionCode: 45,
             "softwareKeyboardLayoutMode": "resize",
             adaptiveIcon: {
                 foregroundImage: "./assets/images/icon-white.png",
